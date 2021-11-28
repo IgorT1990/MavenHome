6c027ed279e3f1cbda9337f9a30c6d5bc909252e
IgorT1990 <igortaranukha1990@gmail.com>
2021-11-28 21:10:51 +0200

    Tried to create methods and reuse them, but not successful(

--- src/automation/lesson5/BasicCalculator.java
+++ src/automation/lesson5/BasicCalculator.java
@@ -3,41 +3,15 @@ package automation.lesson5;
 
 public class BasicCalculator {
     public static void main(String[] args) {
-        System.out.println("Enter your fist number and press Enter");
-        int num1 = DataCalculator.getInt();
 
-        System.out.println("Enter your operator and press Enter");
-        char operator = DataCalculator.getOperator();
+        Operations operations = new Operations();
+        operations.add().multiply().remove().separate();
+        Operations result = operations.remove().add().multiply().separate();
+        System.out.println(result);
 
-        System.out.println("Enter you second number and press Enter");
-        int result = 0;
-        int num2 = DataCalculator.getInt();
-
-        switch (operator) {
-            case '+': {
-                result = num1 + num2;
-                break;
-            }
-            case '-': {
-                result = num1 - num2;
-                break;
-            }
-            case '/': {
-                result = num1 / num2;
-                break;
-            }
-            case '*': {
-                result = num1 * num2;
-                break;
-            }
-            case ('%'): {
-                result = num1 % num2;
-            }
-            default:
-                System.out.println("Something went wrong, try again later");
-        }
-            System.out.println("This is your result: \n" + result);
-
-        }
     }
+
+}
+
+
 
