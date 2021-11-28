13ae3de8e4eacc5ad216deb6d3d2654ffe84a0d2
IgorT1990 <igortaranukha1990@gmail.com>
2021-11-28 21:58:39 +0200

    Update information, but result still displaying 0

--- src/automation/lesson5/BasicCalculator.java
+++ src/automation/lesson5/BasicCalculator.java
@@ -4,11 +4,23 @@ package automation.lesson5;
 public class BasicCalculator {
     public static void main(String[] args) {
 
-        Operations operations = new Operations();
-        operations.add().multiply().remove().separate();
-        Operations result = operations.remove().add().multiply().separate();
-        System.out.println(result);
 
+
+        Operations operations = new Operations();
+        int add = operations.add();
+        System.out.println(add);
+
+        Operations operations1 = new Operations();
+        int remove = operations1.remove();
+        System.out.println(remove);
+
+        Operations operations2 = new Operations();
+        int mul = operations2.multiply();
+        System.out.println(mul);
+
+        Operations operations3 = new Operations();
+        int sep = operations3.separate();
+        System.out.println(sep);
     }
 
 }
