0ef7aa4d1687bc1a23858b85843c4ab92cd77c74
Ihor Taranukha <taranukha@MacBook-Pro-macadmin-4.local>
2021-12-06 13:05:07 +0200

    Home work with logger

---
+++ src/automation/lesson6/Color.java
@@ -0,0 +1,10 @@
+package automation.lesson6;
+
+public class Color {
+
+    public static String GREEN = "\u001B[32m";
+    public static String BLUE = "\u001B[34m";
+    public static String YELLOW = "\u001B[33m";
+    public static String RED = "\u001B[35";
+
+}
